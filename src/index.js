97fc9ea9fb96022b881766fec8c80c9b1da6c111
Renee <156127959+reneexie01@users.noreply.github.com>
2024-07-24 06:52:24 +1000

    Added simple event listeners to toggle between home, menu, and contact

--- src/index.js
+++ src/index.js
@@ -5,8 +5,31 @@ import { contact } from './contact-contents.js';
 
 const div = document.querySelector('#content');
 
+const homeBtn = document.querySelector(".home-button");
+const menuBtn = document.querySelector(".menu-button");
+const contactBtn = document.querySelector(".contact-button");
+
 div.appendChild(logo());
+div.appendChild(description());
+div.appendChild(hours());
 
-div.appendChild(contact());
+homeBtn.addEventListener("click", () => {
+    div.innerHTML = '';
+    div.appendChild(logo());
+    div.appendChild(description());
+    div.appendChild(hours());
+})
 
+menuBtn.addEventListener("click", () => {
+    div.innerHTML = '';
+    div.appendChild(logo());
+    div.appendChild(beverages());
+    div.appendChild(mains());
+    div.appendChild(sides());
+})
 
+contactBtn.addEventListener("click", () => {
+    div.innerHTML = '';
+    div.appendChild(logo());
+    div.appendChild(contact());
+})
