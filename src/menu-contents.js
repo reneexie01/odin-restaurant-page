eeef57b5f89b7da34de374f402ab4f7f4109d36c
Renee <156127959+reneexie01@users.noreply.github.com>
2024-07-25 06:38:25 +1000

    Minor changes to test github pages

--- src/menu-contents.js
+++ src/menu-contents.js
@@ -8,22 +8,22 @@ function beverages() {
     title.innerHTML = 'Beverages';
 
     const beer = document.createElement('h3');
-    beer.innerHTML = 'Beer - $12';
+    beer.innerHTML = 'Beer - $12.50';
     const beerDescription = document.createElement('p');
     beerDescription.innerHTML = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
 
     const wine = document.createElement('h3');
-    wine.innerHTML = 'Wine - $16';
+    wine.innerHTML = 'Wine - $16.90';
     const wineDescription = document.createElement('p');
     wineDescription.innerHTML = 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
 
     const cocktails = document.createElement('h3');
-    cocktails.innerHTML = 'Cocktails - $22';
+    cocktails.innerHTML = 'Cocktails - $22.50';
     const cocktailsDescription = document.createElement('p');
     cocktailsDescription.innerHTML = 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.'
 
     const soda = document.createElement('h3');
-    soda.innerHTML = 'Soda - $10';
+    soda.innerHTML = 'Soda - $9.90';
     const sodaDescription = document.createElement('p');
     sodaDescription.innerHTML = 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
 
@@ -52,22 +52,22 @@ function mains() {
     title.innerHTML = 'Mains';
 
     const pasta = document.createElement('h3');
-    pasta.innerHTML = 'Pasta - $22';
+    pasta.innerHTML = 'Pasta - $22.10';
     const pastaDescription = document.createElement('p');
     pastaDescription.innerHTML = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
 
     const steak = document.createElement('h3');
-    steak.innerHTML = 'Steak - $27';
+    steak.innerHTML = 'Steak - $27.50';
     const steakDescription = document.createElement('p');
     steakDescription.innerHTML = 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
 
     const pizza = document.createElement('h3');
-    pizza.innerHTML = 'Pizza - $24';
+    pizza.innerHTML = 'Pizza - $24.00';
     const pizzaDescription = document.createElement('p');
     pizzaDescription.innerHTML = 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.'
 
     const fish = document.createElement('h3');
-    fish.innerHTML = 'Fish - $29';
+    fish.innerHTML = 'Fish - $29.90';
     const fishDescription = document.createElement('p');
     fishDescription.innerHTML = 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
 
@@ -97,22 +97,22 @@ function sides() {
     title.innerHTML = 'Sides';
 
     const salad = document.createElement('h3');
-    salad.innerHTML = 'Salad - $12';
+    salad.innerHTML = 'Salad - $12.50';
     const saladDescription = document.createElement('p');
     saladDescription.innerHTML = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
 
     const wedges = document.createElement('h3');
-    wedges.innerHTML = 'Wedges - $16';
+    wedges.innerHTML = 'Wedges - $16.00';
     const wedgesDescription = document.createElement('p');
     wedgesDescription.innerHTML = 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
 
     const steamedVeg = document.createElement('h3');
-    steamedVeg.innerHTML = 'Steamed Vegetables - $17';
+    steamedVeg.innerHTML = 'Steamed Vegetables - $14.50';
     const steamedVegDescription = document.createElement('p');
     steamedVegDescription.innerHTML = 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.'
 
     const friedCalamari = document.createElement('h3');
-    friedCalamari.innerHTML = 'Fried Calamari - $21';
+    friedCalamari.innerHTML = 'Fried Calamari - $21.00';
     const friedCalamariDescription = document.createElement('p');
     friedCalamariDescription.innerHTML = 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
 
